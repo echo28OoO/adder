09ef988f1d8ea35724b29492e561b44f9db29621
Joe <yu.xue.work@outlook.com>
2023-06-16 13:49:08 +0800

    feat: panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Guess {
+    value: i32,
 }
 
-pub fn greeting(name: &str) -> String {
-    format!("Hello {}!", name)
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -11,20 +25,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn exploration() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Sunface");
-        let target = "孙飞";
-        assert!(
-            result.contains(target),
-            "你的问候中并没有包含目标姓名 {} ，你的问候是 `{}`",
-            target,
-            result
-        );
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
