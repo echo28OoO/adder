9ff836163b156e0ad3b93d3a3a05b378bb9bb483
Joe <yu.xue.work@outlook.com>
2023-06-16 14:12:40 +0800

    feat:

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,40 @@ impl Guess {
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    // #[ignore]  忽略关键字
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
+
+/*
+    cargo test -- --test-threads=1 指定顺序性，想要顺序运行，值必须为 1
+    cargo test -- --show-output 想要输出 println 需要添加参数
+    cargo test -- --ignored 运行被忽略的测试函数
+*/
