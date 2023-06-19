2799bf2af4888f602fe57f71b1362d4287ceecd4
Joe <yu.xue.work@outlook.com>
2023-06-19 14:07:37 +0800

    feat: bench start

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,24 @@
-pub struct Guess {
-    value: i32,
-}
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            );
-        }
+#![feature(test)]
 
-        Guess { value }
-    }
-}
-
-fn prints_and_returns_10(a: i32) -> i32 {
-    println!("I got the value {}", a);
-    10
-}
-
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+extern crate test;
 
 pub fn add_two(a: i32) -> i32 {
-    internal_adder(a, 2)
-}
-
-fn internal_adder(a: i32, b: i32) -> i32 {
-    a + b
+    a + 2
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // #[test]
-    // fn it_works() -> Result<(), String> {
-    //     if 2 + 2 == 4 {
-    //         Ok(())
-    //     } else {
-    //         Err(String::from("two plus two does not equal four"))
-    //     }
-    // }
-
-    // #[test]
-    // fn this_test_will_pass() {
-    //     let value = prints_and_returns_10(4);
-    //     assert_eq!(10, value);
-    // }
-
-    // // #[ignore]  忽略关键字
-    // #[test]
-    // fn this_test_will_fail() {
-    //     let value = prints_and_returns_10(8);
-    //     assert_eq!(5, value);
-    // }
-
-    use pretty_assertions::assert_eq; // 该包仅能用于测试
+    use test::Bencher;
 
     #[test]
-    fn test_add() {
-        assert_eq!(add(2, 3), 5);
+    fn it_works() {
+        assert_eq!(4, add_two(2));
     }
 
+    #[bench]
+    fn bench_add_two(b: &mut Bencher) {
+        b.iter(|| add_two(2));
+    }
 }
-
-/*
-    cargo test -- --test-threads=1 指定顺序性，想要顺序运行，值必须为 1
-    cargo test -- --show-output 想要输出 println 需要添加参数
-    cargo test -- --ignored 运行被忽略的测试函数
-*/
